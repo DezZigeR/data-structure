17805c303379f1b439d5895d78cf13f74b00d8f0
DezZigeR <dezziger@mail.ru>
2021-03-14 15:02:37 +0300

    Add files via upload
    1. Реализовать метод расширения для поиска количество символов в строке;
    
    2. Требуется подсчитать, сколько раз каждый элемент встречается в данной коллекции.

---
+++ Extension methods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_5
+{
+    class Extension
+    {
+
+        
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа поиска количества символов в строке \n Посчитаем сколько раз встречается буква <И> в тексте : \n <Реализовать метод расширения для поиска количество символов в строке> ");
+            
+            string s = "Реализовать метод расширения для поиска количество символов в строке";
+            char c = 'и';
+            int i = s.CharCount(c);
+            Console.WriteLine(i);
+
+            Console.Read();
+        }
+    }
+
+    public static class StringExtension
+    {
+        public static int CharCount(this string str, char c)
+        {
+            int counter = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == c)
+                    counter++;
+            }
+            return counter;
+        }
+    }
+}
---
+++ List T.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace lesson_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long start, end;
+            start = DateTime.Now.Ticks;
+            GenerateData(15000000);
+            end = DateTime.Now.Ticks;
+            Console.Write("Time of data generation: {0} sec", new TimeSpan(end - start));
+            Console.ReadLine();
+            IEnumerable<uint> data;
+            start = DateTime.Now.Ticks;
+            using (var readFile = File.OpenRead("ip.txt"))
+            using (var bufferedStream = new BufferedStream(readFile, 1048576))
+            using (var reader = new StreamReader(bufferedStream))
+                data = GetIPAddress(reader).ToList();
+            end = DateTime.Now.Ticks;
+            Console.Write("Data is being read: {0} sec", new TimeSpan(end - start));
+            Console.ReadLine();
+            start = DateTime.Now.Ticks;
+            data.GroupBy(element => element)
+                .OrderBy(element => element.LongCount())
+                .Reverse()
+                .ToList()
+                .ForEach(element => Console.WriteLine("Ip: {0}, Count: {1}", new IPAddress(element.Key), element.Count()));
+            end = DateTime.Now.Ticks;
+            Console.WriteLine("Time data: {0} sec", new TimeSpan(end - start));
+            Console.ReadLine();
+        }
+
+        public static IEnumerable<uint> GetIPAddress(StreamReader reader)
+        {
+            const string pattern = "(?<address>(\\d{1,3}\\.{0,1}){4})";
+            const string address = "address";
+            while (!reader.EndOfStream)
+            {
+                var data = reader.ReadLine();
+                var ipString =
+                    Regex.Match(data, pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline)
+                         .Groups[address].Value;
+                var ipBytes = IPAddress.Parse(ipString).GetAddressBytes();
+                yield return BitConverter.ToUInt32(ipBytes, 0);
+            }
+        }
+
+        public static void GenerateData(int count)
+        {
+            var random = new Random();
+            using (var writeFile = File.OpenWrite("ip.txt"))
+            using (var bufferedStream = new BufferedStream(writeFile, 1048576))
+            using (var writer = new StreamWriter(bufferedStream))
+            {
+                for (int i = 0; i < count; i++)
+                {
+                    if (i < count - 1)
+                        writer.WriteLine("10.1.{0}.{1}", random.Next(0, 3), random.Next(1, 254));
+                    else
+                        writer.Write("10.1.{0}.{1}", random.Next(0, 3), random.Next(1, 254));
+                }
+            }
+        }
+    }
+}
